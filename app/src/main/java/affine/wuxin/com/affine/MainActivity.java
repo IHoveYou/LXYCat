5c92f8e4795ffd27642d54ddc73d35db00d1d2cb
楼翔宇 <1255297954@qq.com>
2019-04-25 17:32:15 +0800

    第二次修改

--- app/src/main/java/affine/wuxin/com/affine/MainActivity.java
+++ app/src/main/java/affine/wuxin/com/affine/MainActivity.java
@@ -3,6 +3,8 @@ package affine.wuxin.com.affine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.tany.base.utils.StringUtil;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
